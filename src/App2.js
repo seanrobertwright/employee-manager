fefe84d5d29874f44f117a05c3b51afc0e210f61
Sean Wright <seanrobertwright@gmail.com>
2021-05-13 15:44:40 -0400

    boop

--- src/App2.js
+++ src/App2.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import '@ui5/webcomponents-icons/dist/Assets.js';
 import Navigation from './components/Navigation';
-import {    
-            FlexibleColumnLayout, 
-            Page } from "@ui5/webcomponents-react";
-            import {
-                Container,
-                Header,
-                Body,
-                Content,
-                Aside,
-                Footer
-              } from 'react-holy-grail-layout';
+import {
+            Container,
+            Header,
+            Body,
+            Content,
+            Aside,
+            Footer
+        } from 'react-holy-grail-layout';
 
-import MainHeader from './Header';
+import MainHeader from './components/Header';
 
 class App extends Component {
     render() {
